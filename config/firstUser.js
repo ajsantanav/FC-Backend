179bd02634e5819df5c16c524e80714a60e9c6ab
ajsantanav <adriansantana.v@gmail.com>
2025-01-30 14:44:34 -0500

    working on hash

--- config/firstUser.js
+++ config/firstUser.js
@@ -1,5 +1,9 @@
 const userInfo = [
- 
+    {
+        name: "Adrian Santana",
+        email: "adriansantana.v@gmail.com",
+        password: "Password!1234?",
+    },
 ];
 
 module.exports = userInfo
