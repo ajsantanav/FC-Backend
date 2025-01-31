a8ebec9fed600dfcb8f40afede0889845ede7d59
ajsantanav <adriansantana.v@gmail.com>
2025-01-31 09:07:03 -0500

    base routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,67 @@
 const express = require('express');
-const ruoter = express.Router();
-
+const router = express.Router();
+const Users = require("../models/userSchema");
 ///C.R.U.D.
 
+//get all users?
+router.get('/', async (req, res) => { 
+    try {
+        const allUsers = await Users.find({}) //mongodb query
+        res.json(allUsers)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 
 
 /// Create
+router.post('/', async (req, res) => { 
+    try {
+        const createUser = await Users.create(req.body)
+        console.log(req.body)
+        res.json(createUser)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 
 /// Read
-
-
+router.get('/:id', async (req, res) => {
+    try {
+        const singleUser = await Users.findById(req.params.id)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 /// Update
 
-
+router.update('/:id', async (req, res) => {
+    try {
+        const updateUser = await Users.findByIdAndUpdate(req.params.id, req.body)
+        res.json(updateUser)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 ///Delete
+
+router.delete('/:id', async () => {
+
+    try {
+        const deleteUser = await Users.findByIdAndDelete(req.params.id)
+        res.json(deleteUser);
+    }
+    catch (err) { 
+        res.status(500).json({ message: err.message })
+    }
+
+})
+
+
+module.exports = router;
