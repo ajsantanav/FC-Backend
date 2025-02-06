952e883a448bbde377f1b78d6e8e1f6a44379303
ajsantanav <adriansantana.v@gmail.com>
2025-02-06 00:44:58 -0500

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = express();
 import connect from './config/db.js';
 connect();
+import cors from 'cors';
 import userRoutes from './routes/routes.js';
 import firstUser from './config/seed.js';
 import User from './models/userSchema.js'
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 8080;
 
 // ##############################################
 app.use(express.json()) // this always goes before routes
+app.use(cors());
 app.use('/api/users', userRoutes) //add routes
 app.use('/api/auth', authRoutes);
 // ##############################################
